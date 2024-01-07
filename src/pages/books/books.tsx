b15ee88bb5400e3cad4230afe4d3232d00fcad2b
Сергей Римша <Sergey.Rimsha.ru@gmail.com>
2024-01-07 23:40:30 +0300

    #1 add book-card view mode GRID or LIST

--- src/pages/books/books.tsx
+++ src/pages/books/books.tsx
@@ -1,19 +1,30 @@
 import { ReactElement } from 'react';
 
+import cn from 'clsx';
+
 import s from './books.module.scss';
 
 import { cards } from '@/entities/stateCards.ts';
 import { FilterMenu } from '@/features';
+import { useAppSelector } from '@/shared/lib';
 import { BookCard } from '@/widgets';
 
 export const Books = (): ReactElement => {
+  const view = useAppSelector(state => state.books?.view);
+
+  const styleView = cn(
+    s.books__wrap,
+    { [s.books__wrap_grid]: view === 'GRID' },
+    { [s.books__wrap_list]: view === 'LIST' },
+  );
+
   return (
     <div className={s.books}>
       <FilterMenu />
       <div className={s.books__main}>
-        <section className={s.books__wrap}>
+        <section className={styleView}>
           {cards.map(item => {
-            return <BookCard key={item.id} {...item} />;
+            return <BookCard view={view} key={item.id} {...item} />;
           })}
         </section>
       </div>
--- src/widgets/book-card/book-card.tsx
+++ src/widgets/book-card/book-card.tsx
@@ -1,18 +1,39 @@
 import { ReactElement } from 'react';
 
+import cn from 'clsx';
+
 import s from './book-card.module.scss';
 
+import { BooksState } from '@/entities/books';
 import { CardI } from '@/entities/stateCards.ts';
 import { Rating } from '@/features';
 import { DefaultImgBook } from '@/shared/assets';
 import { Button, Typography } from '@/shared/ui';
 
-export const BookCard = ({ title, description, image, booking }: CardI): ReactElement => {
+interface PropsBookCard extends CardI {
+  view?: BooksState['view'];
+}
+
+export const BookCard = ({ title, description, image, booking, view = 'GRID' }: PropsBookCard): ReactElement => {
+  const cardStyle = cn(s.card, { [s.grid]: view === 'GRID' }, { [s.list]: view === 'LIST' });
+
+  const cardButton = (): ReactElement => {
+    return (
+      <div className={s.card__button}>
+        <Button className={s.card__button_size} variant="primary" size="sm">
+          <Typography className={s.card__button_textSize} variant="body-sm" as="span">
+            {booking.message}
+          </Typography>
+        </Button>
+      </div>
+    );
+  };
+
   return (
-    <div className={s.card}>
+    <div className={cardStyle}>
       <div className={s.card__img}>{image[0] ? <img src={image[0]} alt="book" /> : <DefaultImgBook />}</div>
       <div className={s.card__wrap}>
-        <Rating className={s.card__stars} />
+        {view === 'GRID' && <Rating className={s.card__rating} />}
         <div className={s.card__wrap}>
           <Typography className={s.card__title} variant="sub-sm">
             {title}
@@ -21,13 +42,13 @@ export const BookCard = ({ title, description, image, booking }: CardI): ReactEl
             {description}
           </Typography>
         </div>
-        <div className={s.card__button}>
-          <Button className={s.card__button_size} variant="primary" size="sm">
-            <Typography className={s.card__button_textSize} variant="body-sm" as="span">
-              {booking.message}
-            </Typography>
-          </Button>
-        </div>
+        {view === 'GRID' && cardButton()}
+        {view === 'LIST' && (
+          <div className={s.card__block}>
+            <Rating className={s.card__rating} />
+            {cardButton()}
+          </div>
+        )}
       </div>
     </div>
   );

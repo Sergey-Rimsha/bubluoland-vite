42e098050cadc0e113c1a73827a5392849dbf877
Сергей Римша <Sergey.Rimsha.ru@gmail.com>
2024-01-19 15:14:25 +0300

    #1 add style breadcrumbs, and boolInfo one bloke

--- src/pages/book-info/book-info.tsx
+++ src/pages/book-info/book-info.tsx
@@ -1,5 +1,48 @@
 import { ReactElement } from 'react';
 
+import s from './book-info.module.scss';
+
+import { Rating } from '@/features';
+import { BookImg, Button, Typography } from '@/shared/ui';
+
 export const BookInfo = (): ReactElement => {
-  return <div>book info page</div>;
+  return (
+    <div className={s.bookInfo}>
+      <div className={s.bookInfo__wrap}>
+        <BookImg variant="preview" />
+        <div className={s.bookInfo__block}>
+          <Typography variant="h3" as="h3">
+            Грокаем алгоритмы. Иллюстрированное пособие для программистов и любопытствующих
+          </Typography>
+          <Typography className={s.bookInfo__description} variant="h5" as="h5">
+            Адитья Бхаргава, 2019
+          </Typography>
+          <Button className={s.bookInfo__button} variant="primary" size="lg">
+            Забронировать
+          </Button>
+          <Typography className={s.bookInfo__subTitle} variant="h5" as="h5">
+            О книге
+          </Typography>
+          <Typography className={s.bookInfo__about} variant="body-lg" as="div">
+            Алгоритмы — это всего лишь пошаговые алгоритмы решения задач, и большинство таких задач уже были кем-то
+            решены, протестированы и проверены. Можно, конечно, погрузится в глубокую философию гениального Кнута,
+            изучить многостраничные фолианты с доказательствами и обоснованиями, но хотите ли вы тратить на это свое
+            время? Откройте великолепно иллюстрированную книгу и вы сразу поймете, что алгоритмы — это просто. А грокать
+            алгоритмы — это веселое и увлекательное занятие.
+          </Typography>
+        </div>
+      </div>
+      <div className={s.rating}>
+        <Typography className={s.rating__title} variant="h5" as="h5">
+          Рейтинг
+        </Typography>
+        <div className={s.rating__block}>
+          <Rating />
+          <Typography variant="h5" as="h5">
+            4.3
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/widgets/breadcrumbs/breadcrumbs.tsx
+++ src/widgets/breadcrumbs/breadcrumbs.tsx
@@ -2,6 +2,21 @@ import { ReactElement } from 'react';
 
 import s from './breadcrumbs.module.scss';
 
+import { Typography } from '@/shared/ui';
+
 export const Breadcrumbs = (): ReactElement => {
-  return <div className={s.breadcrumbs}>breadcrumbs</div>;
+  return (
+    <div className={s.breadcrumbs}>
+      <Typography className={s.breadcrumbs__item} variant="body-sm" as="span">
+        Бизнес книги
+      </Typography>
+      <Typography className={s.breadcrumbs__item} variant="body-sm" as="span">
+        /
+      </Typography>
+      <Typography className={s.breadcrumbs__item} variant="body-sm" as="span">
+        Грокаем алгоритмы. Иллюстрированное пособие для программистов и любопытствующих
+      </Typography>
+      <div className={s.breadcrumbs__bg} />
+    </div>
+  );
 };

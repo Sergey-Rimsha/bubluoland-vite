ecd6aac5eca5d9f99b0f19a1279df0a81225fbf4
Sergey <Sergey.rimsha.ru@gmail.com>
2024-06-30 11:15:04 +0300

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bubluoland',
+  appName: 'bubluoland',
+  webDir: 'dist',
+};
+
+export default config;
